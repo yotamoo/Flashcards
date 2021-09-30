076c903f5aaa609ea6fcef6de17253045be276a0
Yotam Ohayon <yotam.ohayon@gorillasapp.com>
2021-09-30 11:48:22 +0200

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Yotam Ohayon on 30.09.21.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
