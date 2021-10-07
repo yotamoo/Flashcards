1e960a2b15a13b849df14543b70ab2694044f8ce
Justyna Kleczar <justyna@Justynas-MBP.home>
2021-10-07 11:36:57 +0200

    Implement repository and service

--- Flashcards/Repositories/FlashcardRepository.swift
+++ Flashcards/Repositories/FlashcardRepository.swift
@@ -9,15 +9,53 @@ import Foundation
 import Combine
 
 protocol FlashcardRepositoryType {
-    var flashcards: AnyPublisher<[FlashcardModel], Error> { get }
+    
+    func getFlashcards() -> AnyPublisher<[FlashcardModel], Error>
 }
 
-class FlashcardRepository: FlashcardRepositoryType {
-    var flashcards: AnyPublisher<[FlashcardModel], Error> {
-        Just([
-            FlashcardModel(front: "asdf", back: "asdf")
-        ])
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+struct FlashcardRepository: FlashcardRepositoryType {
+    
+    private let service: FlashcardServiceProtocol
+    
+    private var userDefaults: UserDefaults
+    
+    init() {
+        self.service = FlashcardService()
+        self.userDefaults = UserDefaults.standard
+    }
+    
+    private func saveToUserDefaults(_ flashcards: [FlashcardModel]) {
+        for flashcard in flashcards {
+            userDefaults.set(flashcard.back, forKey: flashcard.front)
+        }
+    }
+    
+    private func fetchFromUserDefaults() -> [FlashcardModel] {
+        
+        var results: [FlashcardModel] = []
+        let allValues = userDefaults.dictionaryRepresentation()
+        
+        for flashcard in allValues {
+            guard let back = flashcard.value as? String else { return [] }
+            results.append(FlashcardModel(front: flashcard.key, back: back))
+        }
+            
+        return results
+    }
+    
+    func getFlashcards() -> AnyPublisher<[FlashcardModel], Error> {
+        service.getFlashcards { [weak self] result in
+            switch result {
+            case .success(let flashcards):
+                return Just(flashcards)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            case .failure:
+                let flashcardModels = fetchFromUserDefaults()
+                return Just(flashcards)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            }
+        }
     }
 }
--- Flashcards/Services/FlashcardService.swift
+++ Flashcards/Services/FlashcardService.swift
@@ -8,6 +8,17 @@
 import Foundation
 import Combine
 
-protocol FlashcardServiceType {
-    var flashcards: AnyPublisher<[FlashcardModel], Error> { get }
+protocol FlashcardServiceProtocol {
+    
+    func getFlashcards(completion: @escaping (Result<[FlashcardModel], Error>) -> Void)
+}
+
+class FlashcardService: FlashcardServiceProtocol {
+    
+    func getFlashcards(completion: @escaping (Result<[FlashcardModel], Error>) -> Void) {
+        
+        completion(.success([FlashcardModel(front: "der Hund", back: "dog"),
+                             FlashcardModel(front: "die Katze", back: "cat"),
+                             FlashcardModel(front: "die Maus", back: "mouse")]))
+    }
 }
