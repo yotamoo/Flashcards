bb8fa22079ee6f7419a7e59bdc3193ae546cb313
Yotam Ohayon <yotam.ohayon@gorillasapp.com>
2021-10-10 20:00:12 +0200

    added progress bar

--- Flashcards/Views/DeckView.swift
+++ Flashcards/Views/DeckView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 import Combine
 
 class DeckViewViewModel: ObservableObject {
+    @Published var progress: Double = 0
     var cancellable: AnyCancellable?
     var flashcardModels: [FlashcardModel] = []
+    var index: Int = 0
     
     init(flashcardRepository: FlashcardRepositoryType) {
         cancellable = flashcardRepository.getFlashcards().prefix(1).sink { _ in
@@ -24,6 +26,10 @@ class DeckViewViewModel: ObservableObject {
     func cardViewed(_ success: Bool, model: FlashcardModel) {
         print(success ? "well done" : "next time")
         
+        index += 1
+        
+        progress = Double(index) / Double(flashcardModels.count) * 100
+        
         if model == flashcardModels.first {
             print("finished")
         }
@@ -31,15 +37,19 @@ class DeckViewViewModel: ObservableObject {
 }
 
 struct DeckView: View {
-    let viewModel = DeckViewViewModel(flashcardRepository: FlashcardRepository())
+    @ObservedObject var viewModel = DeckViewViewModel(flashcardRepository: FlashcardRepository())
     
     var body: some View {
-        ZStack {
-            ForEach(viewModel.flashcardModels) { model in
-                FlashcardView(model: model) {
-                    viewModel.cardViewed($0, model: model)
+        return VStack {
+            ZStack {
+                ForEach(viewModel.flashcardModels) { model in
+                    FlashcardView(model: model) {
+                        viewModel.cardViewed($0, model: model)
+                    }
                 }
             }
+            ProgressBarView(progress: viewModel.progress)
+                .frame(width: 400, height: 30)
         }
     }
 }
