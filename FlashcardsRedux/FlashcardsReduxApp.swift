fee949ef500e40f6a36820476b3dc0f4854ec69d
Yotam Ohayon <yotam.ohayon@gorillasapp.com>
2021-10-27 13:19:37 +0200

    added target

---
+++ FlashcardsRedux/FlashcardsReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsReduxApp.swift
+//  FlashcardsRedux
+//
+//  Created by Yotam Ohayon on 19.10.21.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
